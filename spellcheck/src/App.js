521d5cdc560d2dc994435d38b042d85c73a211de
unknown <nisha.pawar2231@gmail.com>
2024-05-13 13:56:17 +0530

    first commit

---
+++ spellcheck/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheck from './SpellCheck';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheck></SpellCheck>
+    </div>
+  );
+}
+
+export default App;
